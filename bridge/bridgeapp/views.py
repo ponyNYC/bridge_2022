20b524ad53c1db772066d0d496fb717ffb77636a
rlee794 <rlee97@gmail.com>
2022-05-10 19:18:34 -0500

    adding routes!
    as a team, with kayla & dustin, created
    * URLs
    * Views names
    * CSS stylesheet

--- bridge/bridgeapp/views.py
+++ bridge/bridgeapp/views.py
@@ -1,3 +1,4 @@
+from ast import Pass
 import re
 from django.shortcuts import render
 from django.views import View
@@ -15,3 +16,63 @@ class BridgeHomeView(View):
            },
        ) 
 # Create your views here.
+
+class BridgeCategoryView(View):
+    def get(self, request):
+       categories = Category.objects.all()
+
+       return render(
+           request=request,
+           template_name='categories.html',
+           context={
+               'categories': categories,
+           },
+       ) 
+
+class BridgeThreadView(View):
+    def get(self, request):
+       categories = Category.objects.all()
+
+       return render(
+           request=request,
+           template_name='thread.html',
+           context={
+               'categories': categories,
+           },
+       ) 
+class BridgeResponseView(View):
+    def get(self, request):
+       categories = Category.objects.all()
+
+       return render(
+           request=request,
+           template_name='categories.html',
+           context={
+               'categories': categories,
+           },
+       ) 
+
+
+class BridgeCreateView(View):
+    def get(self, request):
+       categories = Category.objects.all()
+
+       return render(
+           request=request,
+           template_name='categories.html',
+           context={
+               'categories': categories,
+           },
+       ) 
+
+class BridgeUpdateView(View):
+    def get(self, request):
+       categories = Category.objects.all()
+
+       return render(
+           request=request,
+           template_name='categories.html',
+           context={
+               'categories': categories,
+           },
+       ) 
