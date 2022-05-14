5d37fa1085572e40aa16dbb1ca155d384cce4954
Bryan Garrett <bgarrett.work@gmail.com>
2022-05-14 11:27:36 -0500

    Completed BridgeThreadView * Completed BridgeThreadView * Created thread.html * Updated urls to conform to the convention suggested by Chung * Updated categories.html to test slugify function * Updated BridgeCategoryView to add slug attribute and save function * Updated name of stlyes file to styles.css * Added some styles to styles.css to add some basic styling to thread.html

--- bridge/bridgeapp/urls.py
+++ bridge/bridgeapp/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 from bridgeapp.views import BridgeHomeView, BridgeCategoryView, BridgeThreadView, BridgeResponseView, BridgeCreateView, BridgeUpdateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.template.defaultfilters import slugify
 
 urlpatterns = [
     path('', BridgeHomeView.as_view(), name='bridge'),
     path('categories/', BridgeCategoryView.as_view(), name='categories'),
-    path('thread/', BridgeThreadView.as_view(), name='thread'),
+    path('threads/<str:category_slug>', BridgeThreadView.as_view(), name='threads'),
     path('response/', BridgeResponseView.as_view(), name='response'),
-    path('createthread/', BridgeCreateView.as_view(), name='create'),
+    path('threads/new', BridgeCreateView.as_view(), name='new_thread'),
     path('updateresponse/', BridgeUpdateView.as_view(), name='update'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
