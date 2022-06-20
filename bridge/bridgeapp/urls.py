db65c40f1e6abb3929751164dfee7f1b8dcaac30
Bryan Garrett <bgarrett.work@gmail.com>
2022-06-20 18:28:47 -0500

    limited access to homepage only until user logs in

--- bridge/bridgeapp/urls.py
+++ bridge/bridgeapp/urls.py
@@ -3,16 +3,17 @@ from .views import BridgeHomeView, BridgeCategoryView, BridgeThreadView
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     # home page
     path('', BridgeHomeView.as_view(), name='home'),
     # single category page
     path('categories/<int:category_id>/<str:category_slug>',
-         BridgeCategoryView.as_view(), name='category'),
+         login_required(BridgeCategoryView.as_view()), name='category'),
     # single thread page
     path('threads/<int:thread_id>/<int:resp_id>',
-         BridgeThreadView.as_view(), name='thread'),
+         login_required(BridgeThreadView.as_view()), name='thread'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # add static assets
