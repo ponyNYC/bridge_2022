4625dc55a4c12d479e343f4f68428e01afe47aa6
Chung Kao <chungckao@gmail.com>
2022-07-31 21:48:47 +0800

    Correct typo in .main-message seciton in home page and add event listener to modal in thread.html page

--- bridge/bridgeapp/static/js/response.js
+++ bridge/bridgeapp/static/js/response.js
@@ -1,27 +1,30 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  // Pop the model in thread.html if the ResponseForm
-  // <textarea> (response.body) is preloaded with data
-  const respForm = document.getElementById('response-form');
-  const respText = respForm.body.value;
-  const showModalBtn = document.getElementById('response-form-btn');
+    // Pop the model in thread.html if the ResponseForm
+    // <textarea> (response.body) is preloaded with data
+    const respForm = document.getElementById("response-form");
+    const respText = respForm.body.value;
+    const showModalBtn = document.getElementById("response-form-btn");
 
-  if (respText) {
-    showModalBtn.click();
-    const modalHeader = document.querySelector('.modal-header');
-    const modalFooter = document.querySelector('.modal-footer');
-    const closeBtn = document.querySelector('.btn-close');
-    const cancelBtn = document.getElementById('btn-cancel');
-    const link = document.createElement('a');
-    link.className = "btn btn-close";
-    link.href = respForm.action.slice(0,-1) + '0'
-    const subBtn = document.createElement('a');
-    subBtn.className = "btn btn-secondary";
-    subBtn.textContent = "Cancel";
-    subBtn.href = respForm.action.slice(0,-1) + '0'
-    modalHeader.removeChild(closeBtn);
-    modalHeader.append(link);
-    modalFooter.removeChild(cancelBtn);
-    modalFooter.insertBefore(subBtn, modalFooter.firstChild)
-    // console.log(modalHeader, modalFooter, closeBtn, cancelBtn);
-  }
+    if (respText) {
+        showModalBtn.click();
+        const modalContent = document.querySelector("modal-content");
+        const modalHeader = document.querySelector(".modal-header");
+        const modalFooter = document.querySelector(".modal-footer");
+        const closeBtn = document.querySelector(".btn-close");
+        const cancelBtn = document.getElementById("btn-cancel");
+        const link = document.createElement("a");
+        link.className = "btn btn-close";
+        link.href = respForm.action.slice(0, -1) + "0";
+        const subBtn = document.createElement("a");
+        subBtn.className = "btn btn-secondary";
+        subBtn.textContent = "Cancel";
+        subBtn.href = respForm.action.slice(0, -1) + "0";
+        modalHeader.removeChild(closeBtn);
+        modalHeader.append(link);
+        modalFooter.removeChild(cancelBtn);
+        modalFooter.insertBefore(subBtn, modalFooter.firstChild);
+        modalContent.addEventListener("click", (e) => {
+            if (e.target.tagName === "A") respForm.body.value = "";
+        });
+    }
 });
